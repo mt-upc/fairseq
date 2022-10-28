a95271d6f3b78a3c6f82a504b112c34cac07da07
Ioannis Tsiamas <ioannis.tsiamas@gmail.com>
2022-10-28 15:41:03 +0200

    make stride an argument in Conv1dSubsampler

--- fairseq/models/speech_to_text/s2t_transformer.py
+++ fairseq/models/speech_to_text/s2t_transformer.py
@@ -33,7 +33,6 @@ class Conv1dSubsampler(nn.Module):
     """Convolutional subsampler: a stack of 1D convolution (along temporal
     dimension) followed by non-linear activation via gated linear units
     (https://arxiv.org/abs/1911.08460)
-
     Args:
         in_channels (int): the number of input channels
         mid_channels (int): the number of intermediate channels
@@ -47,15 +46,17 @@ class Conv1dSubsampler(nn.Module):
         mid_channels: int,
         out_channels: int,
         kernel_sizes: List[int] = (3, 3),
+        stride: int = 2
     ):
         super(Conv1dSubsampler, self).__init__()
         self.n_layers = len(kernel_sizes)
+        self.stride = stride
         self.conv_layers = nn.ModuleList(
             nn.Conv1d(
                 in_channels if i == 0 else mid_channels // 2,
                 mid_channels if i < self.n_layers - 1 else out_channels * 2,
                 k,
-                stride=2,
+                stride=self.stride,
                 padding=k // 2,
             )
             for i, k in enumerate(kernel_sizes)
@@ -75,7 +76,11 @@ class Conv1dSubsampler(nn.Module):
             x = nn.functional.glu(x, dim=1)
         _, _, out_seq_len = x.size()
         x = x.transpose(1, 2).transpose(0, 1).contiguous()  # -> T x B x (C x D)
-        return x, self.get_out_seq_lens_tensor(src_lengths)
+        if self.stride == 1:
+            out_src_lengths = src_lengths
+        else:
+            out_src_lengths = self.get_out_seq_lens_tensor(src_lengths)
+        return x, out_src_lengths
 
 
 @register_model("s2t_transformer")
