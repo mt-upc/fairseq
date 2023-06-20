a430425c4402df18756bfde16468d6eccbab07a8
Ioannis Tsiamas <ioannis.tsiamas@gmail.com>
2023-06-20 15:29:15 +0200

    [UPD] Added layernorm for the special embedding (for consistency with the speech features before concatenation), added argument for the type of positional embeddings (learned=mbart, not-learned=nllb), added a fixed scaling factor of 7.0 to increase the importance of the features compared to the positional embeddings (the scale difference is now similar to the nllb features.norm / pos.norm)

--- fairseq/modules/speech_embedder.py
+++ fairseq/modules/speech_embedder.py
@@ -16,6 +16,12 @@ class EmbedderConfig(FairseqDataclass):
     use_positional_embedding: bool = field(
         default=False, metadata={"help": "use positional embedding"}
     )
+    is_learned: bool = field(
+        default=False, metadata={"help": "use learned positional embedding"}
+    )
+    scale_embedding: bool = field(
+        default=False, metadata={"help": "scale embeddings by sqrt(dimension)"}
+    )
     embed_dim: int = field(
         default=1024, metadata={"help": "embedding dimension"}
     )
@@ -25,11 +31,8 @@ class EmbedderConfig(FairseqDataclass):
     padding_idx: int = field(
         default=1, metadata={"help": "padding index"}
     )
-    path: str = field(
-        default="", metadata={"help": "path for pre-trained embedder checkpoint"}
-    )
-    freeze: bool = field(
-        default=False, metadata={"help": "freeze embedder parameters"}
+    layer_norm_special: bool = field(
+        default=False, metadata={"help": "use layer norm for special embedding"}
     )
 
 
@@ -41,15 +44,20 @@ class Embedder(nn.Module):
         if cfg.use_special_embedding:
             self.bos_emb = nn.Parameter(torch.zeros(cfg.embed_dim))
             self.eos_emb = nn.Parameter(torch.zeros(cfg.embed_dim))
+            if cfg.layer_norm_special:
+                self.layernorm_special = LayerNorm(cfg.embed_dim)
         if cfg.use_positional_embedding:
             self.pos_emb = PositionalEmbedding(
                 cfg.max_source_positions,
                 cfg.embed_dim,
                 cfg.padding_idx,
-                learned=True,
+                learned=cfg.is_learned,
             )
-            self.layernorm = LayerNorm(cfg.embed_dim)
-            
+            if cfg.is_learned:
+                self.layernorm = LayerNorm(cfg.embed_dim)
+        self.pre_scale = 7.0 if cfg.scale_embedding else 1.0
+        # self.post_scale = nn.Parameter(torch.ones(cfg.embed_dim))
+        
     def forward(self, x, padding_mask=None):
         """Add special embedding and positional embedding.
         Args:
@@ -59,23 +67,33 @@ class Embedder(nn.Module):
             x (FloatTensor): (B, T+2, C)
             padding_mask (ByteTensor): (B, T+2)
         """
-        
         B = x.size(0)
         lengths = get_lengths(x.transpose(0, 1), padding_mask)
         assert B == len(lengths)
         
         if self.cfg.use_special_embedding:
-            x = torch.cat([self.bos_emb.view(1, 1, -1).expand(B, 1, -1), x], dim=1)
+            # prepend bos
+            if hasattr(self, "layernorm_special"):
+                bos_emb = self.layernorm_special(self.bos_emb)
+                eos_emb = self.layernorm_special(self.eos_emb)
+            else:
+                bos_emb = self.bos_emb
+                eos_emb = self.eos_emb
+            x = torch.cat([bos_emb.view(1, 1, -1).expand(B, 1, -1), x], dim=1)
             lengths += 1
+            # append padding (zeros) and then convert first padding to eos
             x = torch.cat([x, torch.zeros(B, 1, x.size(-1), device=x.device, dtype=x.dtype)], dim=1)
             for i in range(B):
-                x[i, lengths[i], :] = self.eos_emb
+                x[i, lengths[i], :] = eos_emb
             lengths += 1
             
             padding_mask = lengths_to_padding_mask(lengths)
         
+        x *= self.pre_scale
+        
         if self.cfg.use_positional_embedding:
             x = x + self.pos_emb(padding_mask.long())
-            x = self.layernorm(x)
+            if hasattr(self, "layernorm"):
+                x = self.layernorm(x)
             
         return x, padding_mask, lengths
